4afaf2c4b5c5a325f50f30fac0eccadd3cf45997
churikova-olga <olga_churikova890@mail.ru>
2022-08-08 14:19:19 +0300

    005-TypeScript

--- library/dist/middleware/error.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (req, res) => {
-    res.status(404);
-    const content = '404 | not found';
-    res.send(content);
-};
--- library/src/infra/container.ts
+++
@@ -1,9 +0,0 @@
-import {Container} from "inversify";
-import { BooksRepository } from './BooksRepository';
-import "reflect-metadata";
-
-const container = new Container();
-
-container.bind(BooksRepository).to(BooksRepository).inSingletonScope();
-
-export {container};
